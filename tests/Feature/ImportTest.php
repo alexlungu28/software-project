e36786d3696bb853d3a1757984ab492dedb9fc12
Luca Becheanu <l.becheanu@student.tudelft.nl>
2021-05-12 01:11:50 +0200

    Added test for importing functionality

---
+++ tests/Feature/ImportTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Http\UploadedFile;
+use Maatwebsite\Excel\Facades\Excel;
+use Tests\TestCase;
+
+class ImportTest extends TestCase
+{
+    /**
+     * Test to verify a user can import the list of students.
+     *
+     * @return void
+     */
+    public function testUserCanImportStudents()
+    {
+        $file = UploadedFile::fake()->create('ImportTest.csv');
+
+        Excel::fake();
+
+        $response = $this->post(route('import'), [
+            'fileToUpload' => $file
+        ]);
+        $response->assertRedirect('');
+    }
+}
