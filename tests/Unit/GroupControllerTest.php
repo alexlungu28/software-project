f40a84b519cd94a49e7294f368fa1380dad08609
Alexandru Lungu <A.Lungu@student.tudelft.nl>
2021-05-28 16:09:36 +0300

    Added tests for GroupController

---
+++ tests/Unit/GroupControllerTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\CourseEditionUser;
+use App\Models\Group;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+
+class GroupControllerTest extends TestCase
+{
+    use withoutMiddleware;
+    use RefreshDatabase;
+
+    /**
+     * Insert the specified entries inside the database tables.
+     */
+    private function before() {
+        Auth::shouldReceive('id')->andReturn(1);
+        Group::insert(
+            [
+                'group_name' => 'Group 1',
+                'content' => 'First group',
+                'grade' => 10,
+                'course_edition_id' => 1
+            ]
+        );
+    }
+
+    /**
+     * Test to verify that the correct view is returned according to user role.
+     */
+    public function testViewLecturer() {
+        $this->before();
+        CourseEditionUser::insert(
+            [
+                'user_id' => 1,
+                'course_edition_id' => 1,
+                'role' => 'lecturer'
+            ]
+        );
+        $response = $this->get('/group/1');
+        $response->assertViewIs('weeks');
+    }
+
+    /**
+     * Test to verify that the correct view is returned according to user role.
+     */
+    public function testViewTA() {
+        $this->before();
+        CourseEditionUser::insert(
+            [
+                'user_id' => 1,
+                'course_edition_id' => 1,
+                'role' => 'TA'
+            ]
+        );
+        $response = $this->get('/group/1');
+        $response->assertViewIs('weeksTA');
+    }
+
+    /**
+     * Test to verify that the correct view is returned upon accessing the route.
+     */
+    public function testViewWeek() {
+        $this->before();
+        $response = $this->get('/group/1/week/1');
+        $response->assertViewIs('week');
+    }
+}
