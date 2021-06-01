91efbca874c126f77c1d6a9bc3a1b55e81efd952
Luca Becheanu <l.becheanu@student.tudelft.nl>
2021-06-01 18:46:16 +0300

    Updated first version of importing TAs

--- resources/views/import.blade.php
+++ resources/views/import.blade.php
@@ -10,17 +10,33 @@
             Import Export CSV
         </div>
         <div class="card-body">
-            <form action="{{ route('import', [$edition_id]) }}" method="POST" enctype="multipart/form-data">
+            <form action="" method="POST" enctype="multipart/form-data">
                 @csrf
                 <input type="file" name="file" class="form-control">
                 <br>
-                <button class="btn btn-success">Import Students from the CSV found at Brightspace>Grades>Export</button>
-                <a class="btn btn-warning" href="{{ route('export', [$edition_id]) }}">Export All User Data from the Platform</a>
+                <button class="btn btn-success" >Import Students from the CSV found at Brightspace>Grades>Export</button>
+                <a class="btn btn-danger" href="{{ route('export', [$edition_id]) }}">Export All User Data from the Platform</a>
             </form>
         </div>
-        <img src="{{ URL::to('/assets/images/Brightspace_Export_Grade.png') }}">
+
     </div>
 </div>
-
+<div class="container">
+    <div class="card bg-light mt-3">
+        <div class="card-header">
+            Import Export CSV
+        </div>
+        <div class="card-body">
+            <form action="{{ route('importTA', [$edition_id]) }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <input type="file" name="file" class="form-control">
+                <br>
+                <button class="btn btn-success">Import TAs from a CSV file</button>
+            </form>
+        </div>
+        <img src="{{ URL::to('/assets/images/TA.png') }}">
+    </div>
+    <img src="{{ URL::to('/assets/images/Brightspace_Export_Grade.png') }}">
+</div>
 </body>
 @endsection
