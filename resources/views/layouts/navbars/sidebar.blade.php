69e8e56e353c4acd1593308f72350258ba016a04
Toma Zamfirescu <t.zamfirescu@student.tudelft.nl>
2021-06-06 21:49:52 +0200

    Added interventions to the sidebar.

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -35,6 +35,14 @@
                 <p>{{ __('Groups') }}</p>
             </a>
         </li>
+
+        <li class="nav-item{{ $activePage == 'interventions' ? ' active' : '' }}">
+            <a class="nav-link" href={{ route('interventions', [$edition_id]) }}>
+                <i class="material-icons">interventions</i>
+                <p>{{ __('Interventions') }}</p>
+            </a>
+        </li>
+
         <li class="nav-item{{ $activePage == 'userList' ? ' active' : '' }}">
             <a class="nav-link" href="{{ route('studentList', [$edition_id]) }}">
                 <i class="material-icons">S</i>
