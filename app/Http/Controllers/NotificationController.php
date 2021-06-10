30142fc5333d557f28a791039dea8ade99869bc6
Alexandru Lungu <A.Lungu@student.tudelft.nl>
2021-06-10 18:43:28 +0300

    Fixed codestyle and phpmd

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use Carbon\Carbon;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Routing\Controller as BaseController;
 
-class NotificationController extends Controller
+
+class NotificationController extends BaseController
 {
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
     /**
      * Display a listing of the resource.
      *
@@ -85,31 +92,28 @@ class NotificationController extends Controller
         //
     }
 
-    public function view($editionId) {
+    public function view($editionId)
+    {
         $interventions = DB::table('course_edition_user')->where('course_edition_id', '=', $editionId)->get()
             ->flatMap(function ($editionUser) {
-               $userInterventions = DB::table('interventions_individual')
-                   ->where('user_id', '=', $editionUser->user_id)->get();
-               $currentDate = Carbon::now();
-               $passed = $userInterventions->map(function ($intervention) use ($currentDate) {
-                   //ddd($intervention->end_day);
-                   if ($currentDate->gt($intervention->end_day)) {
-                       return $intervention;
-                   }
-                   return null;
-               })->filter(function ($intervention) {
-                   return $intervention != null;
-               })->unique();
-               return $passed;
+                $userInterventions = DB::table('interventions_individual')
+                    ->where('user_id', '=', $editionUser->user_id)->get();
+                $currentDate = Carbon::now();
+                $passed = $userInterventions->map(function ($intervention) use ($currentDate) {
+                    if ($currentDate->gt($intervention->end_day)) {
+                        return $intervention;
+                    }
+                    return null;
+                })->filter(function ($intervention) {
+                    return $intervention != null;
+                })->unique();
+                return $passed;
             })->filter(function ($intervention) {
                 return $intervention != null;
             })->unique();
-        //ddd($interventions);
         $users = $interventions->map(function ($intervention) {
             return DB::table('users')->where('id', '=', $intervention->user_id)->get()->first();
         });
-        //ddd($users[0]);
-        //ddd($interventions);
         return view('pages.notifications', [
             'edition_id' => $editionId,
             'interventions' => $interventions,
