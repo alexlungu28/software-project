44f8d8547c3b646514628fea94c9d5ca18dc082e
Ciurba Claudiu Alexandru <C.Ciurba@student.tudelft.nl>
2021-06-11 00:07:48 +0200

    Added a new chart to the weeks specific to a group. They contain a table with all the students within a group and for now it displays a random chart which will be changed to be from git inspector.

--- resources/views/weeks.blade.php
+++ resources/views/weeks.blade.php
@@ -1,7 +1,42 @@
 @extends('layouts.app', ['activePage' => 'group', 'titlePage' => __('Weeks')])
+<?php
 
+$dataPoints = array(
+    array("label"=>"Chrome", "y"=>64.02),
+    array("label"=>"Firefox", "y"=>12.55),
+    array("label"=>"IE", "y"=>8.47),
+    array("label"=>"Safari", "y"=>6.08),
+    array("label"=>"Edge", "y"=>4.29),
+    array("label"=>"Others", "y"=>4.59)
+)
+
+?>
 @section('content')
-    <div class="content">
+    <head>
+        <script>
+            window.onload = function() {
+
+                var chart = new CanvasJS.Chart("chartContainer", {
+                    animationEnabled: true,
+                    title: {
+                        text: "Usage Share of Desktop Browsers"
+                    },
+                    subtitles: [{
+                        text: "November 2017"
+                    }],
+                    data: [{
+                        type: "pie",
+                        yValueFormatString: "#,##0.00\"%\"",
+                        indexLabel: "{label} ({y})",
+                        dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+                    }]
+                });
+                chart.render();
+
+            }
+        </script>
+    </head>
+    <div class="content" style="display: flex">
         <div class="container-fluid">
             <button type="submit" name="update" class="btn btn-dark rounded-pill" onclick="window.location='{{ route('groups', ['edition_id'=>$edition_id]) }}'">Back!</button>
             <div class="row">
@@ -18,7 +53,7 @@
                 @endfor
             </div>
             <div class="row">
-                <div class="col-lg-6 col-md-12">
+                <div class="col-lg-12 col-md-12">
                     <div class="card">
                         <div class="card-header card-header-primary">
                             <h4 class="card-title">Problem cases</h4>
@@ -84,6 +119,64 @@
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+        @if(DB::table('gitanalyses')->where('group_id', "=", $group_id)->exists())
+                <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+                <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+        @endif
+            <div class="row">
+                <div class="col-lg-12 col-md-12">
+                    <div class="card">
+                        <div class="card-header card-header-primary">
+                            <h4 class="card-title">Students</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="tab-content">
+                                <div class="tab-pane active" id="profile">
+                                    <table class="table">
+                                        <thead class="text-primary">
+                                        <th>Netid</th>
+                                        <th>Last Name</th>
+                                        <th>First Name</th>
+                                        <th>Email</th>
+                                        </thead>
+                                        <tbody>
+                                        @foreach($users as $user)
+                                            @if(DB::table('course_edition_user')->where("user_id","=", $user->user_id)->where('role','=','student')->exists())
+                                            <tr>
+                                                <td>
+                                                    {{DB::table('users')
+                                                            ->where('id', '=', $user->user_id)
+                                                            ->value('net_id')}}
+                                                </td>
+                                                <td>
+                                                    {{DB::table('users')
+                                                            ->where('id', '=', $user->user_id)
+                                                            ->value('last_name')}}
+                                                </td>
+                                                <td>
+                                                    {{DB::table('users')
+                                                            ->where('id', '=', $user->user_id)
+                                                            ->value('first_name')}}
+                                                </td>
+                                                <td>
+                                                    {{DB::table('users')
+                                                            ->where('id', '=', $user->user_id)
+                                                            ->value('email')}}
+                                                </td>
+                                            </tr>
+                                            @endif
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
