5bb5b748111741bce387aa9a55f7c3a8f13fa2a9
Toma Zamfirescu <t.zamfirescu@student.tudelft.nl>
2021-06-16 09:34:27 +0200

    Changed the format of the interventions table to keep consistency in the app. Filters and search bar are included in the table now.

--- resources/views/interventions/interventions_all_tab.blade.php
+++ resources/views/interventions/interventions_all_tab.blade.php
@@ -1,17 +1,29 @@
 <div class="tab-pane fade show active" id="allInterventions" role="tabpanel" aria-labelledby="pills-allInterventions-tab">
-
+    <button type="button" name="createIntervention" class="btn btn-danger rounded-pill" data-toggle="modal" data-target="{{"#createIntervention" . $edition_id}}" style="float:right">Create Intervention</button>
     <div class="table-responsive">
 
-        <h3>Interventions</h3>
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" />
+        <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+        <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+        <script>
+            $(document).ready( function () {
+                $('#tableInterventions').DataTable();
+            } );
 
-        <button type="button" name="createIntervention" class="btn btn-danger rounded-pill" data-toggle="modal" data-target="{{"#createIntervention" . $edition_id}}" style="float:right">Create Intervention</button>
+        </script>
+
+
 
         <div class="card">
+            <div class="card-header card-header-primary">
+                <input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>">
+                <h4 class="card-title ">Interventions</h4>
+            </div>
 
-            <div class="card-block table-border-style">
+            <div class="card-body">
                 <div class="table-responsive">
 
-                    <table class="table table-hover" style="table-layout:fixed;">
+                    <table class="table table-hover" id="tableInterventions" style="table-layout:fixed;">
                         <thead>
                         <tr>
                             <th style="width:15%">Name</th>
