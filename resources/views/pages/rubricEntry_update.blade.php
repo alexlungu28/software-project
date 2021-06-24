71376e6f61aafad7f6be8b4a63c87ca623a82445
MSchoenmaker <M.Schoenmaker@student.tudelft.nl>
2021-06-24 11:03:10 +0200

    Changed rubricEntryUpdate to a popup instead of a separate page

--- resources/views/pages/rubricEntry_update.blade.php
+++ resources/views/pages/rubricEntry_update.blade.php
@@ -1,31 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Rubric Management</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-</head>
-<body>
+<div class="modal fade" id="{{"updateEntry" . $rubricEntry->id}}">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" style="align-content: center">Update rubric entry</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span></button>
+            </div>
 
-<div class="container">
-    <h2 class="text-center">Rubric Entry Management | Update</h2>
-    <br>
-    <form action = "/rubricEntryUpdate" method = "post" class="form-group" style="width:70%; margin-left:15%;" action="/action_page.php">
+            <div class="modal-body">
+                <div class="box-body">
+                    <div class="form-group">
+                        <form action = "/rubricEntryUpdate" method = "post" class="form-group" style="width:70%; margin-left:15%;" action="/action_page.php">
+                            <input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>">
+                            <input type = "hidden" name = "id" value ="{{$rubricEntry->id}}">
+                            @method('PUT')
+                            <label>New Description</label>
+                            <input type="text" class="form-control" placeholder="{{$rubricEntry->description}}" name="description">
 
-        <input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>">
-        <input type = "hidden" name = "id" value ="{{$rubricEntry->id}}">
+                            <button type="submit"  value = "Add" class="btn btn-primary">Submit update</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
 
-        @method('PUT')
-        <label class="form-group"></label>
-
-        <label>New Description</label>
-        <input type="text" class="form-control" placeholder="{{$rubricEntry->description}}" name="description">
-
-        <button type="submit"  value = "Add" class="btn btn-primary">Submit</button>
-
-    </form>
+            <div class="modal-footer">
+                <button type="button" id="close" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                <script type="text/javascript">
+                    $(window).on('load', function (e) {
+                        $('#close').on('click', function (e) {
+                            $("#div").load(" #div > *");
+                        });
+                    });
+                </script>
+            </div>
+        </div>
+    </div>
 </div>
